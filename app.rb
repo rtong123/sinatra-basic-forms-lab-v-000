ab8190d1391b49e83e7b70db20e0efc4b6a6fb05
Rebecca Tong <rtong@buffalo.edu>
2018-07-11 18:58:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+  erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppyform' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    erb :display_puppy
+  end
 end
